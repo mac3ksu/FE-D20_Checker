c10f8d975666e23419e5e56cdc7131571f2f7b1c
CarliLakin <clbishop@burnsmcd.com>
2017-06-29 16:18:40 -0500

    Finished b023
    Finished editing b023--DNP DCA

--- D20MPP_XML_Functions.py
+++ D20MPP_XML_Functions.py
@@ -36,6 +36,7 @@ def b023_check(app):
     #   <table "B023_CFG">
     #   <table "B023_DEV">
     #   <table "B023_PNT">
+    #   There is no B023_POL
 
     print('B023 - DNP DCA')
     print('\t', 'B023_PNT')
@@ -46,7 +47,7 @@ def b023_check(app):
         b023_pnt_list.append((record[0].get('Field_Value'), record[1].get('Field_Value')))
 
     print('\t', 'B023_POL')
-    b023_pol_list = []
+    #b023_pol_list = []
     print('\t\t D20 DNP DCA does not have a POL list')
     # for i, record in enumerate(app[3]):
     #     print('\t\t', 'Record', ':', i)
@@ -62,40 +63,47 @@ def b023_check(app):
     print('\t', 'B023_DEV')
     b023_dev_list = []
     for i, record in enumerate(app[1]):
+        #Application Address
         print('\t\t', record[0].get('Field_Name'), ':', record[0].get('Field_Value'))
+        #Poll Interval (s)
+        print('\t\t\t', record[5][0][0][3].get('Field_Name'), ':', record[5][0][0][3].get('Field_Value'))
+        #Integrity Poll Interval
+        print('\t\t\t', record[6].get('Field_Name'), ':', record[6].get('Field_Value'))
         #Offline After Fail
         print('\t\t\t', record[3][0][0][8].get('Field_Name'), ':', record[3][0][0][8].get('Field_Value'))
         #Failures For Bad Channel
-        print('\t\t\t', record[3][0][0][4].get('Field_Name'), ':', record[3][0][0][7].get('Field_Value'))
+        print('\t\t\t', record[7].get('Field_Name'), ':', record[7].get('Field_Value'))
         #Time Syncing
-        print('\t\t\t', record[3][0][0][4].get('Field_Name'), ':', record[3][0][0][7].get('Field_Value'))
+        print('\t\t\t', record[3][0][0][4].get('Field_Name'), ':', record[3][0][0][4].get('Field_Value'))
         #Data Link CFM Required
         print('\t\t\t', record[3][0][0][7].get('Field_Name'), ':', record[3][0][0][7].get('Field_Value'))
-        print('\t\t\t', record[4].get('Field_Name'), ':', record[4].get('Field_Value'))
-        print('\t\t\t', record[6].get('Field_Name'), ':', record[6].get('Field_Value'))
-        print('\t\t\t', record[8][0][0][2].get('Field_Name'), ':', record[8][0][0][2].get('Field_Value'))
-        print('\t\t\t', record[8][0][0][3].get('Field_Name'), ':', record[8][0][0][3].get('Field_Value'))
-        for index in range(int(record[8][0][0][2].get('Field_Value')),
-                           int(record[8][0][0][2].get('Field_Value')) + int(
-                                   record[8][0][0][3].get('Field_Value'))):
+        #First Point Record
+        print('\t\t\t', record[9][0][0][2].get('Field_Name'), ':', record[9][0][0][2].get('Field_Value'))
+        #Number of Point Records
+        print('\t\t\t', record[9][0][0][3].get('Field_Name'), ':', record[9][0][0][3].get('Field_Value'))
+        for index in range(int(record[9][0][0][2].get('Field_Value')),
+                           int(record[9][0][0][2].get('Field_Value')) + int(
+                                   record[9][0][0][3].get('Field_Value'))):
             print('\t\t\t\t', b023_pnt_list[index])
-        print('\t\t\t', record[8][0][0][4].get('Field_Name'), ':', record[8][0][0][4].get('Field_Value'))
-        print('\t\t\t', record[8][0][0][5].get('Field_Name'), ':', record[8][0][0][5].get('Field_Value'))
-        for index in range(int(record[8][0][0][4].get('Field_Value')),
-                           int(record[8][0][0][4].get('Field_Value')) + int(
-                                   record[8][0][0][5].get('Field_Value'))):
-            print('\t\t\t\t', b023_pol_list[index])
-        print('\t\t\t', record[9][0][0][5].get('Field_Name'), ':', record[9][0][0][5].get('Field_Value'))
+        # There are no POLL Records for the D20 DNP DCA
+        # print('\t\t\t', record[8][0][0][4].get('Field_Name'), ':', record[8][0][0][4].get('Field_Value'))
+        # print('\t\t\t', record[8][0][0][5].get('Field_Name'), ':', record[8][0][0][5].get('Field_Value'))
+        # for index in range(int(record[9][0][0][4].get('Field_Value')),
+        #                    int(record[9][0][0][4].get('Field_Value')) + int(
+        #                            record[9][0][0][5].get('Field_Value'))):
+        #     print('\t\t\t\t', b023_pol_list[index])
+        # print('\t\t\t', record[9][0][0][5].get('Field_Name'), ':', record[9][0][0][5].get('Field_Value'))
         b023_dev_list.append(record[0].get('Field_Value'))
 
     print('\t' 'B023_CFG')
     for i, record in enumerate(app[0]):
+        #DCA Address
         print('\t\t', record[1].get('Field_Name'), ':', record[1].get('Field_Value'))
-        print('\t\t\t', record[3].get('Field_Name'), ':', record[3].get('Field_Value'))
-        print('\t\t\t', record[2][0][0][0].get('Field_Name'), ':', record[2][0][0][0].get('Field_Value'))
+        #Restart Delay
+        print('\t\t\t', record[2].get('Field_Name'), ':', record[2].get('Field_Value'))
         print('\t\t\t', 'Devices in DCA:')
-        for index in range(int(record[10].get('Field_Value')),
-                           int(record[10].get('Field_Value')) + int(record[11].get('Field_Value'))):
+        for index in range(int(record[8].get('Field_Value')),
+                           int(record[8].get('Field_Value')) + int(record[9].get('Field_Value'))):
             print('\t\t\t\t', b023_dev_list[index])
     return
 
