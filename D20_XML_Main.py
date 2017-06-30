fa715b6ab23670e42ee51286444c8963a7bc900d
mac3ksu <mac3ksu@users.noreply.github.com>
2017-06-30 13:00:31 -0500

    added part number check
    Script now checks the 'Part_Number' attribute instead of 'Device_Type' to determine the D20 model. Carli will need to import her D20M++ and D20ME functions into this main py file and write code to execute the check

--- D20_XML_Main.py
+++ D20_XML_Main.py
@@ -4,15 +4,24 @@ import xml.etree.ElementTree as et
 
 
 if __name__ == '__main__':
-    filename = 'LORAIN_H.xml'
-    file_dir = os.path.expanduser(os.path.join('~', 'Documents', 'GitHub', 'FE-D20_Checker', 'Example D20 XML', 'D20MX'))
+    filename = 'OAKDAL2G D20ME.xml'
+    file_dir = os.path.expanduser(os.path.join('~', 'Documents', 'GitHub', 'FE-D20_Checker', 'Example D20 XML', 'D20MEII'))
     tree = et.parse(os.path.join(file_dir, filename))
     root = tree.getroot()
 
     print(filename)
 
-    # with open(os.path.join(directory, xml_filename[:-4]+'_Check.txt')) as of:
-    if root[0][0].get('Device_Type') == 'D20MX':
+    if root[0][0][1][0].get('Part_Number') == '526-1006':
+        # This is a D20M++
+        print('D20M++')
+    elif root[0][0][1][0].get('Part_Number') == '526-3001':
+        # This is a D20MX
         d20mx_check(filename, file_dir)
+    elif root[0][0][1][0].get('Part_Number') == '526-2007 CCU':
+        # This is a D20ME
+        print('D20ME')
+
+    #if root[0][0].get('Device_Type') == 'D20MX':
+    #    d20mx_check(filename, file_dir)
 
 
