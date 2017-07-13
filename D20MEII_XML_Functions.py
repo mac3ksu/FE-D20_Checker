ccfde7eff41eb2415cde18ddf032d5e88779356e
CarliLakin <clbishop@burnsmcd.com>
2017-07-13 13:25:03 -0500

    Merged the WinPt Check to the D20MEII_XML_Functions.py

--- D20MEII_XML_Functions.py
+++ D20MEII_XML_Functions.py
@@ -1,11 +1,67 @@
 import xml.etree.ElementTree as ET
 import xlrd
 import os
-from WinPt_Check_Function import winpt_check
 
 def set_comlist():
   global b013_com_list # Needed to modify global copy of globvar
   b013_com_list = []
+
+def winpt_check(xcel_filename, directory, app, column, table_num, type):
+
+    try:
+        # Counters for WinPt status printing
+        count = 0
+
+        filepath = directory + '/' + xcel_filename
+
+        wbook = xlrd.open_workbook(filepath)
+
+        for sheet in wbook.sheet_names():
+            if 'Sheet1' in sheet:
+                wsheet_name = sheet
+
+        wsheet = wbook.sheet_by_name(wsheet_name)
+
+        # General Point Check
+        print('\t\t', type, 'Points Check')
+        for i, record in enumerate(app[table_num]):
+            xl_value = str(wsheet.cell_value(i + 2, column))
+            check_value = record[0].get('Field_Value')
+            if check_value[4] == '0':
+                if check_value[5] == '0':
+                    if xl_value[0] == (check_value[6]):
+                        pass
+                        # print('\t\t\t\t', check_value[6], ':', xl_status[0], '<status> WinPts match')
+                    else:
+                        print('\t\t\t', 'DNP Point', i, '<',
+                              type, '> WinPt does not match the points list. Please refer to the SGConfig.')
+                        count = count + 1  # Indicates that a status WinPt does not match
+                else:
+                    if xl_value[0] + xl_value[1] == (check_value[5] + check_value[6]):
+                        pass
+                        # print('\t\t\t\t', check_value[5] + check_value[6], ':', xl_status[0] + xl_status[1], '<status> WinPts match')
+                    else:
+                        print('\t\t\t', 'DNP Point', i, '<',
+                              type, '> WinPt does not match the points list. Please refer to the SGConfig.')
+                        count = count + 1  # Indicates that a status WinPt does not match
+            else:
+                if xl_value[0] + xl_value[1] + xl_value[2] == (check_value[4] + check_value[5] + check_value[6]):
+                    pass
+                    # print('\t\t\t\t', check_value[4] + check_value[5] + check_value[6], ':', xl_status[0] + xl_status[1]
+                    #      + xl_status[2], '<status> WinPts match')
+                else:
+                    print('\t\t\t', 'DNP Point', i, '<',
+                              type, '> WinPt does not match the points list. Please refer to the SGConfig.')
+                    count = count + 1  # Indicates that a status WinPt does not match
+
+        # If all status WinPts match, print statement
+        if count == 0:
+            print('\t\t\t', 'All <', type, '> WinPts match.')
+        else:
+            pass
+
+    except Exception:
+        print('\t\t\t', 'Error: Cannot find the file.')
 
 def d20meII_check(xml_filename, directory):
     tree = ET.parse(os.path.join(directory, xml_filename))
@@ -315,7 +371,7 @@ def b021_check(app):
 
         # Compare the Winpoints from the points list to what's programmed in the D20
 
-        # Put in the path to the XCEL template file
+        # Put in the path to the excel template file
         directory = os.path.expanduser(os.path.join('~', 'Documents', 'GitHub', 'FE-D20_Checker', 'Example D20 XML', 'D20MEII'))
 
         # Determine that the XCEL template's filename is D20 DNP Map WinPt Check
@@ -344,6 +400,7 @@ def b021_check(app):
                         analog_index = i
                     elif cell.value == 'CONTROL':
                         control_index = i
+
             except Exception:
                 print('\t\t\t', 'Error: Cannot find the file.')
 
